b5369f9b5cbca5417f6b722693cd9003d946b933
ByeongHoon <eep7942@gmail.com>
2016-06-09 02:47:28 +0900

    환승경로 파싱준비

--- tarin/ParseHandle.swift
+++ tarin/ParseHandle.swift
@@ -402,180 +402,141 @@ class ParsingInWeb_RealTimeArrival: NSObject, NSXMLParserDelegate{
 //    
 //    
 //}
-//class ParsingInWeb_Transfer: NSObject, NSXMLParserDelegate{
-//    var parser = NSXMLParser()
-//    
-//    var posts = NSMutableArray()
-//    
-//    var elements = NSMutableDictionary()
-//    var element = NSString()
-//    var parsingData = NSMutableString()
-//    var rootName: String = ""
-//    var childName: String = ""
-//    var returnData: String = ""
-//    
-//    func beginParsing(parsingMode mode: Int)
-//    {
-//        parsingMode = mode
-//        if(mode == 0)
-//        {
-//            d_posts = NSMutableDictionary()
-//            d_posts = [:]
-//        }
-//        else if(mode == 1)
-//        {
-//            a_posts = NSMutableArray()
-//            a_posts = []
-//        }
-//        
-//        parser = NSXMLParser(contentsOfURL: NSURL(fileURLWithPath: path!))!
-//        parser.delegate = self
-//        parser.parse()
-//        
-//    }
-//    
-//    func parser(parser:NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
-//                attributes attributeDict: [String : String])
-//    {
-//        element = elementName
-//        
-//        if(elementName as NSString).isEqualToString("Station")
-//        {
-//            elements = NSMutableDictionary()
-//            elements = [:]
-//            stationCode = NSMutableString()
-//            stationCode = ""
-//            stationExCode = NSMutableString()
-//            stationExCode = ""
-//            stationName = NSMutableString()
-//            stationName = ""
-//            stationLineNum = NSMutableString()
-//            stationLineNum = ""
-//            lineImage = NSString()
-//            lineImage = ""
-//            
-//            
-//        }
-//    }
-//    
-//    func parser(parser: NSXMLParser!, foundCharacters string: String!)
-//    {
-//        if element.isEqualToString("stnCode")
-//        {
-//            stationCode.appendString(string)
-//        }
-//        else if element.isEqualToString("stnName")
-//        {
-//            stationName.appendString(string)
-//        }
-//        else if element.isEqualToString("stnExCode")
-//        {
-//            stationExCode.appendString(string)
-//        }
-//        else if element.isEqualToString("stnLine")
-//        {
-//            let temp = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//            var tempname: String = ""
-//            switch temp {
-//            case "I":
-//                tempname = "인천지하철 1호선"
-//                break
-//                
-//            case "K":
-//                tempname = "경의중앙선"
-//                break
-//                
-//            case "B":
-//                tempname = "분당선"
-//                break
-//                
-//            case "A":
-//                tempname = "공항철도"
-//                break
-//                
-//            case "U":
-//                tempname = "의정부경전철"
-//                break
-//                
-//            case "S":
-//                tempname = "신분당선"
-//                break
-//                
-//            case "SU":
-//                tempname = "수인선"
-//                break
-//                
-//            case "G":
-//                tempname = "경춘선"
-//                break
-//            case "E":
-//                tempname = "에버라인"
-//                break
-//            default:
-//                tempname = temp
-//                break
-//            }
-//            
-//            stationLineNum.appendString(tempname)
-//        }
-//        
-//    }
-//    func parser(parser: NSXMLParser!, didEndElement elementName: String!,namespaceURI: String!, qualifiedName qName: String!)
-//    {
-//        //  if(elementName as NSString).isEqualToString("row")
-//        if(elementName as NSString).isEqualToString("Station")
-//        {
-//            
-//            if !stationName.isEqual(nil)
-//            {
-//                returnData = ""
-//                returnData = (stationName as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                elements.setObject(returnData, forKey: "stnName")
-//            }
-//            if !stationExCode.isEqual(nil)
-//            {
-//                returnData = ""
-//                returnData = (stationExCode as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                // stationExCode.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                //returnData = NSCharacterSet.whitespaceAndNewlineCharacterSet()
-//                elements.setObject(returnData, forKey: "stnExCode")
-//            }
-//            if !stationLineNum.isEqual(nil)
-//            {
-//                returnData = ""
-//                returnData = (stationLineNum as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                elements.setObject(returnData, forKey: "stnLine")
-//                elements.setObject("\(returnData).png", forKey: "stnImage")
-//            }
-//            if !stationCode.isEqual(nil)
-//            {
-//                returnData = ""
-//                returnData = (stationCode as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                elements.setObject(returnData, forKey: "stnCode")
-//            }
-//            
-//            if(parsingMode == 0)
-//            {
-//                d_posts.setObject(elements, forKey: stationName)
-//            }
-//            else if(parsingMode == 1)
-//            {
-//                a_posts.addObject(elements)
-//                let imagename = (stationLineNum as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-//                stations.append(StationInfo(name: (stationName as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()), externalCode: (stationExCode as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()),
-//                    lineNumber: (stationLineNum as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()),
-//                    imageName:("\(imagename).png")
-//                    )
-//                )
-//            }
-//            
-//        }
-//    }
-//    
-//    
-//    
-//}
-//
+class ParsingInWeb_Transfer: NSObject, NSXMLParserDelegate{
+    var parser = NSXMLParser()
+    
+    var posts = NSMutableArray()
+    
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var parsingData = NSMutableString()
+    var rootName: String = ""
+    var childName: String = ""
+    var returnData: String = ""
+    
+    
+    var StartStation = NSMutableString()
+    var DestStation = NSMutableString()
+    var ShortTimeStatonList = NSMutableArray()
+    var ShortTimeTransferInfo = NSMutableString()
+    
+   
+    var ShortTransferStatonList = NSMutableArray()
+    var ShortTransferTransferInfo = NSMutableString()
+    
+    
+    func beginParsing(startStation: String, destStation: String)
+    {
+   
+        posts = NSMutableArray()
+        posts = []
+        let original = String("http://swopenAPI.seoul.go.kr/api/subway/42764d466365657031303157445a4859/xml/shortestRoute/0/5/\(startStation)/\(destStation)")
+        var encodedString = original.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+
+        parser = NSXMLParser(contentsOfURL: NSURL(string: encodedString!)!)!
+        parser.delegate = self
+        parser.parse()
+        
+    }
+    
+    func parser(parser:NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
+                attributes attributeDict: [String : String])
+    {
+        element = elementName
+        
+        if(elementName as NSString).isEqualToString("Station")
+        {
+            elements = NSMutableDictionary()
+            elements = [:]
+            StartStation = NSMutableString()
+            StartStation = ""
+            DestStation = NSMutableString()
+            DestStation = ""
+            ShortTimeStatonList = NSMutableArray()
+            ShortTimeStatonList = []
+            ShortTimeTransferInfo = NSMutableString()
+            ShortTimeTransferInfo = ""
+            
+            ShortTransferStatonList = NSMutableArray()
+            ShortTransferStatonList = []
+            ShortTransferTransferInfo = NSMutableString()
+            ShortTransferTransferInfo = ""
+            
+            
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, foundCharacters string: String!)
+    {
+        if element.isEqualToString("statnFnm")
+        {
+            StartStation.appendString(string)
+        }
+        else if element.isEqualToString("statnTnm")
+        {
+            DestStation.appendString(string)
+        }
+        else if element.isEqualToString("shtStatnNm")
+        {
+            ShortTimeStatonList.addObject(string)
+        }
+        else if element.isEqualToString("shtTravelMsg")
+        {
+             ShortTimeTransferInfo.appendString(string)
+        }
+        else if element.isEqualToString("minStatnNm")
+        {
+            ShortTransferStatonList.addObject(string)
+        }
+        else if element.isEqualToString("minTravelMsg")
+        {
+            ShortTransferTransferInfo.appendString(string)
+        }
+    }
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!,namespaceURI: String!, qualifiedName qName: String!)
+    {
+          if(elementName as NSString).isEqualToString("row")
+          {
+            
+            if !StartStation.isEqual(nil)
+            {
+                elements.setObject(StartStation, forKey: "출발역")
+            }
+            if !DestStation.isEqual(nil)
+            {
+                elements.setObject(DestStation, forKey: "도착역")
+            }
+            if !ShortTimeStatonList.isEqual(nil)
+            {
+                elements.setObject(ShortTimeStatonList, forKey: "최단경로경유역")
+            }
+            if !ShortTimeTransferInfo.isEqual(nil)
+            {
+                elements.setObject(ShortTimeTransferInfo, forKey: "최단경로정보")
+            }
+            if !ShortTransferStatonList.isEqual(nil)
+            {
+                elements.setObject(ShortTransferStatonList, forKey: "최소환승경유역")
+            }
+            if !ShortTransferTransferInfo.isEqual(nil)
+            {
+                 elements.setObject(ShortTransferTransferInfo, forKey: "최소환승정보")
+            }
+                posts.addObject(elements)
+                stations.append(StationInfo(name: (stationName as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()), externalCode: (stationExCode as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()),
+                    lineNumber: (stationLineNum as NSString as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()),
+                    imageName:("\(imagename).png")
+                    )
+                )
+            }
+            
+        }
+    }
+    
+    
+    
+}
+
 
 
 class ParsingInFile: NSObject, NSXMLParserDelegate{
