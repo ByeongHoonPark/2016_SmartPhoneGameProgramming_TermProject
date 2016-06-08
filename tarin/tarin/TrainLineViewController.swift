4af9950be5e56be88854a7a5f0897a6881bb6c44
ByeongHoon <eep7942@gmail.com>
2016-06-09 02:12:48 +0900

    복구후 재커밋

--- tarin/tarin/TrainLineViewController.swift
+++ tarin/tarin/TrainLineViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchResultsUpdating {
+class TrainLineViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate, UISearchResultsUpdating {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
@@ -44,16 +44,51 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
     var filterdData = [StationInfo]()
     var selectedData: StationInfo?
     
+    var StartStationName: String?
+    var DestinationName: String?
+    
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if tableView == StartStationTableView
+        {
+            StartStationName = ParsingData.stations[indexPath.row].name
+        }
+        if tableView == DestinationStationTableView
+        {
+            DestinationName = ParsingData.stations[indexPath.row].name
+        }
+    }
+    
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)->UITableViewCell
     {
         
-        var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        var cell : UITableViewCell = StartStationTableView.dequeueReusableCellWithIdentifier("StartStationCell")!
         if(cell.isEqual(NSNull))
         {
-            cell = NSBundle.mainBundle().loadNibNamed("Cell", owner: self, options: nil)[0] as! UITableViewCell;
+            cell = NSBundle.mainBundle().loadNibNamed("StartStationCell", owner: self, options: nil)[0] as! UITableViewCell;
         }
         let station1: StationInfo
-        if self.searchController.active && self.searchController.searchBar.text != ""{
+        if StartSearchBar.active && StartSearchBar.searchBar.text != ""{
+            station1 = filterdData[indexPath.row]
+        }
+        else
+        {
+            station1 = ParsingData.stations[indexPath.row]
+        }
+        cell.textLabel!.text = station1.name
+        cell.detailTextLabel!.text = station1.lineNumber
+        cell.imageView!.image = UIImage(named:station1.imageName )
+        
+        return cell as UITableViewCell
+        
+        cell = DestinationStationTableView.dequeueReusableCellWithIdentifier("DestinationStationCell")!
+        if(cell.isEqual(NSNull))
+        {
+            cell = NSBundle.mainBundle().loadNibNamed("DestinationStationCell", owner: self, options: nil)[0] as! UITableViewCell;
+        }
+        if DestinationSearchBar.active && DestinationSearchBar.searchBar.text != ""{
             station1 = filterdData[indexPath.row]
         }
         else
@@ -70,7 +105,7 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        if self.searchController.active && self.searchController.searchBar.text != ""{
+        if StartSearchBar.active && StartSearchBar.searchBar.text != ""{
             return filterdData.count
         }
         else
@@ -78,6 +113,15 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
             return ParsingData.a_posts.count
             
         }
+        if DestinationSearchBar.active && DestinationSearchBar.searchBar.text != ""{
+            return filterdData.count
+        }
+        else
+        {
+            return ParsingData.a_posts.count
+            
+        }
+        
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -88,7 +132,7 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        ParsingData.beginParsing(parsingMode: 1)
         StartSearchBar.searchResultsUpdater = self
         StartSearchBar.dimsBackgroundDuringPresentation = false
         StartSearchBar.searchBar.delegate = self
@@ -99,7 +143,8 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
         StartSearchBar.searchBar.searchBarStyle = .Minimal
         StartStationTableView.tableHeaderView = StartSearchBar.searchBar
         StartStationTableView.hidden = true
-
+        StartStationTableView.delegate = self
+        StartStationTableView.dataSource = self
         
         DestinationSearchBar.searchResultsUpdater = self
         DestinationSearchBar.dimsBackgroundDuringPresentation = false
@@ -112,11 +157,13 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
         SearchBoundView2.addSubview(DestinationSearchBar.searchBar)
         DestinationStationTableView.tableHeaderView = DestinationSearchBar.searchBar
         DestinationStationTableView.hidden = true
-        var frame2 = DestinationSearchBar.searchBar.frame
-        frame2.origin.x = 10
-        frame2.origin.y = 0
-        frame2.size.width = 340
-        DestinationSearchBar.searchBar.frame = frame2
+        DestinationStationTableView.delegate = self
+        DestinationStationTableView.dataSource = self
+//        var frame2 = DestinationSearchBar.searchBar.frame
+//        frame2.origin.x = 10
+//        frame2.origin.y = 0
+//        frame2.size.width = 340
+//        DestinationSearchBar.searchBar.frame = frame2
         
     }
     override func didReceiveMemoryWarning() {
@@ -137,9 +184,18 @@ class TrainLineViewController: UIViewController,UISearchBarDelegate, UISearchRes
     }
     func filterContentForSearchText(searchBar: UISearchBar, searchText: String)
     {
-      //  filterdData = ParsingData.stations.filter({(station: StationInfo) -> Bool in
-      //      return station.name.lowercaseString.containsString(searchText.lowercaseString)})
-      //  self.tableView.reloadData()
+        
+        filterdData = ParsingData.stations.filter({(station: StationInfo) -> Bool in
+            return station.name.lowercaseString.containsString(searchText.lowercaseString)})
+        if searchBar == StartSearchBar.searchBar
+        {
+            StartStationTableView.reloadData()
+        }
+        if searchBar == DestinationSearchBar.searchBar
+        {
+            DestinationStationTableView.reloadData()
+        }
+        
         
     }
     func updateSearchResultsForSearchController(searchController: UISearchController) {
