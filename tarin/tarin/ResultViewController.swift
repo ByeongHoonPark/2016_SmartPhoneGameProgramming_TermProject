49818ce5fc599f0acf1f0a535d98a8ad73ea7143
ByeongHoon <eep7942@gmail.com>
2016-06-13 17:17:05 +0900

    열차 애니메이션

--- tarin/tarin/ResultViewController.swift
+++ tarin/tarin/ResultViewController.swift
@@ -27,6 +27,10 @@ class ResultViewController: UIViewController {
     var LineNum: Int?
     var ParsingData = ParsingInWeb_RealTimeArrival()
     var Excode: String = ""
+    
+    
+    var Upward = [RealTimeInfo]()
+    var Downward = [RealTimeInfo]()
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabelText = ((self.tabBarController as? ResultTabBarController)?.nameLabelText)!
@@ -38,20 +42,87 @@ class ResultViewController: UIViewController {
         stationLineNumImage.image = UIImage(named: LineNumImageText)
         UIDesignView.line = LineNum!
         CurrentStationNameLabel.text = nameLabelText
-        
-        print("test")
+ 
         ParsingData.beginParsing(nameLabelText)
         
-        posLabel.text = ParsingData.posts.objectAtIndex(0).valueForKey("도착정보") as! NSString as String
+        Upward = ParsingData.RealTimestations.filter({(station: RealTimeInfo) -> Bool in
+            return station.updnLine == "상행" || station.updnLine == "외선"
+        })
+        Downward = ParsingData.RealTimestations.filter({(station: RealTimeInfo) -> Bool in
+            return station.updnLine == "상행" || station.updnLine == "내선"
+        })
+
+        posLabel.text = Upward[0].arriveStatus
+        print(Upward[0].arriveStatus)
         destLabel.text = ParsingData.posts.objectAtIndex(0).valueForKey("행선지") as! NSString as String
         posLabel2.text = ParsingData.posts.objectAtIndex(2).valueForKey("도착정보") as! NSString as String
         destLabel2.text = ParsingData.posts.objectAtIndex(2).valueForKey("행선지") as! NSString as String
+       // ParsingData.posts.filter(<#T##includeElement: (AnyObject) throws -> Bool##(AnyObject) throws -> Bool#>)
+        
+//        posLabel.text = ParsingData.posts.objectAtIndex(0).valueForKey("도착정보") as! NSString as String
+//        destLabel.text = ParsingData.posts.objectAtIndex(0).valueForKey("행선지") as! NSString as String
+//        posLabel2.text = ParsingData.posts.objectAtIndex(2).valueForKey("도착정보") as! NSString as String
+//        destLabel2.text = ParsingData.posts.objectAtIndex(2).valueForKey("행선지") as! NSString as String
      
     }
     override func viewDidAppear(animated: Bool) {
-        UIView.animateWithDuration(50.0, animations: {
-            self.trainImage.frame = CGRectMake(256.0 - self.trainImage.frame.size.width/2, 281.0, self.trainImage.frame.size.width, self.trainImage.frame.size.height)
-        })
+        if posLabel.text! == "전역 도착"
+        {
+            print(posLabel.text!)
+            var firsttarin = self.trainImage.frame
+            firsttarin.origin.x = 256 - self.trainImage.frame.size.width/2
+            firsttarin.origin.y = 281
+            self.trainImage.frame = firsttarin
+            //self.trainImage
+        }
+        else if posLabel.text! == "전역 출발"
+        {
+            print(posLabel.text!)
+            var firsttarin = self.trainImage.frame
+            firsttarin.origin.x = 256 - self.trainImage.frame.size.width/2
+            firsttarin.origin.y = 281
+            self.trainImage.frame = firsttarin
+            UIView.animateWithDuration(70.0, animations: {
+                self.trainImage.frame = CGRectMake(400.0 - self.trainImage.frame.size.width/2, 281.0, self.trainImage.frame.size.width, self.trainImage.frame.size.height)
+            })
+            //self.trainImage
+        }
+        else if posLabel.text! == "\(stationNameLabel.text!) 도착"
+        {
+            print(posLabel.text!)
+            var firsttarin = self.trainImage.frame
+            firsttarin.origin.x = 512 - self.trainImage.frame.size.width/2
+            firsttarin.origin.y = 281
+            self.trainImage.frame = firsttarin
+            //self.trainImage
+        }
+        else if posLabel.text! == "\(stationNameLabel.text!) 진입"
+        {
+            print(posLabel.text!)
+            var firsttarin = self.trainImage.frame
+            firsttarin.origin.x = 400.0 - self.trainImage.frame.size.width/2
+            firsttarin.origin.y = 281
+            self.trainImage.frame = firsttarin
+            UIView.animateWithDuration(30.0, animations: {
+                self.trainImage.frame = CGRectMake(512 - self.trainImage.frame.size.width/2, 281.0, self.trainImage.frame.size.width, self.trainImage.frame.size.height)
+            })
+            //self.trainImage
+        }
+        else
+        {
+            var firsttarin = self.trainImage.frame
+            firsttarin.origin.x = 100 - self.trainImage.frame.size.width/2
+            firsttarin.origin.y = 281
+            self.trainImage.frame = firsttarin
+            UIView.animateWithDuration(70.0, animations: {
+                self.trainImage.frame = CGRectMake(256.0 - self.trainImage.frame.size.width/2, 281.0, self.trainImage.frame.size.width, self.trainImage.frame.size.height)
+            })
+
+        }
+        
+//        UIView.animateWithDuration(50.0, animations: {
+//            self.trainImage.frame = CGRectMake(256.0 - self.trainImage.frame.size.width/2, 281.0, self.trainImage.frame.size.width, self.trainImage.frame.size.height)
+//        })
     }
 
     
